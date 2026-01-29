2d41dc36cf6b30a993db5b57d8401b21fe329148
vladgermanyuk <vlad.germanyuk@icloud.com>
2026-01-29 23:29:52 +0200

    Refactor after_install.sh to improve container handling

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-# Зупиняємо старий застосунок
-docker stop my-nodejs-app || true
-docker rm my-nodejs-app || true
+APP_NAME=my-app
+IMAGE_URI=547445247510.dkr.ecr.us-east-1.amazonaws.com/my-app:latest
 
-# Запускаємо новий застосунок
-docker run -d --name my-app -p 80:8080 547445247510.dkr.ecr.us-east-1.amazonaws.com/my-app:latest
+# Проверяем, существует ли контейнер и удаляем его
+if [ "$(docker ps -a -q -f name=$APP_NAME)" ]; then
+    echo "Stopping and removing existing container $APP_NAME"
+    docker stop $APP_NAME
+    docker rm $APP_NAME
+fi
+
+# Запускаем новый контейнер
+echo "Running new container $APP_NAME"
+docker run -d --name $APP_NAME -p 80:8080 $IMAGE_URI
